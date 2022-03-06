17247f5f0b39495a7d657ee115b2c98b837b4008
Joeliton Furtado <joeliton-ge@hotmail.com>
2022-03-06 00:11:31 -0300

    acrescentado novas linhas de codigo

--- imc.js
+++ imc.js
@@ -1,8 +1,29 @@
 
 const calcular = document.getElementById('calcular');
 
+function calcularIMC (peso, altura){
+    return peso / (altura * altura).toFixed(2);
+}
 
+function classificacaoIMC(valordoIMC){
+    const classificacao = '';
 
+    if(valordoIMC < 18.5){
+        classificacao = 'abaixo do peso.';
+    } else if (valordoIMC < 25){
+        classificacao = 'com peso ideal. Parabéns :D';
+    } else if (valordoIMC < 30){
+        classificacao = 'levemente acima do peso';
+    } else if (valordoIMC < 35){
+        classificacao = 'com obesidade grau I.';
+    } else if (valordoIMC < 40) {
+        classificacao = 'com obesidade grau II';
+    } else {
+        classificacao = 'com obesidade grau III. Cuidado :x';
+    }
+    
+    return 'testando' + classificacao;
+}
 function imc (){
     const nome = document.getElementById('nome').value;
     const altura = document.getElementById('altura').value;
@@ -10,16 +31,11 @@ function imc (){
     const resultado = document.getElementById('resultado');
 
     if(nome !== '' && altura !== '' && peso !== '') {
-        const valorIMC = (peso / (altura * altura)).toFixed(2);
         
-        let classificacao = '';
-
-        if(valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
-        } else if (valorIMC < 25){
-            classificacao = 'com peso ideal. Parabéns :D';
-        }
+        const valorIMC = calculaIMC(peso, altura)
         
+        let classificacao = classificacaoIMC(valorIMC);
+       
         resultado.textContent = `${nome} seu IMC é ${valorIMC} e você esta ${classificacao}`;
     } else{
         resultado.textContent = 'preencha todos os campos!!!';
